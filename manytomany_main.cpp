fb5bbdc8d11cae8efc103ccc4a5e4ff1151b75fa
laurelb320 <laurelb320@gmail.com>
2025-03-06 10:02:24 -0500

    Add files via upload
    replacing manytomany

--- manytomany_main.cpp
+++ manytomany_main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 1;
   }
-
+  int* myrecvsize = new int[size];
   int sdata_size_in_file[size];
    std::ifstream input_file(argv[1]);
 
@@ -37,7 +37,10 @@ int main(int argc, char *argv[]) {
     assert(size_in_file == size);
 
     /* put the second lines in secondcounts array */
-    for (int i = 0; i < size; ++i) input_file >> sendcounts[i];
+    for (int i = 0; i < size; ++i){
+      input_file >> sendcounts[i];
+      myrecvsize[i] = sendcounts[i];
+    }
 
     sdata_size_in_file[0] = 0;
     for (int i = 0; i < size; i++) sdata_size_in_file[0] += sendcounts[i];
@@ -45,7 +48,10 @@ int main(int argc, char *argv[]) {
     int tmp[size];
     for (int pe = 1; pe < size; pe++) {
       sdata_size_in_file[pe] = 0;
-      for (int i = 0; i < size; i++) input_file >> tmp[i];
+      for (int i = 0; i < size; i++) {
+        input_file >> tmp[i];
+        myrecvsize[i] += tmp[i];
+      }
       for (int i = 0; i < size; i++) sdata_size_in_file[pe] += tmp[i];
       MPI_Send(tmp, size, MPI_INT, pe, 0, MPI_COMM_WORLD);
     }
@@ -90,6 +96,10 @@ int main(int argc, char *argv[]) {
   /* collect the information in PE 0 and check the output with the sample output */
   std::ifstream output_file(argv[2]);
   if (rank == 0) {
+    if(recv_num_custom != myrecvsize[0]){
+      std::cout << "Wrong Output " << std::endl;
+      exit(1);
+    }
     int* sample_outline = new int[recv_num_custom];
   	/* read the first line and and put it in sample_outline vector */
     for (int i = 0; i < recv_num_custom; ++i) output_file >> sample_outline[i];
@@ -106,7 +116,10 @@ int main(int argc, char *argv[]) {
     for (int pe = 1; pe < size; pe++) {
       int remote_recv_count;
       MPI_Recv(&remote_recv_count, 1, MPI_INT, pe, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+      if(remote_recv_count != myrecvsize[pe]){
+        std::cout << "Wrong Output " << std::endl;
+        exit(1);
+      }
       int* remote_recv_data = new int[remote_recv_count];
       MPI_Recv(remote_recv_data, remote_recv_count, MPI_INT, pe, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
@@ -135,7 +148,7 @@ int main(int argc, char *argv[]) {
 
   /* If the code has come this far, output is correct */
   if (rank == 0) std::cout << "Correct Output" << std::endl;
-
+  delete[] myrecvsize;
   /* free all dynamically allocated memory */
   delete[] send_data;
   delete[] sendcounts;
